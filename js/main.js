1b74f8c48bde2c6fef68e869d15e6a4bd9af0a08
weihaoqianjin <2544239995.@qq.com>
2017-11-27 18:37:23 +0800

    stage one over

--- js/main.js
+++ js/main.js
@@ -24,21 +24,25 @@ game.States.boot = function() {
 game.States.preload = function() {
     this.preload = function() {
         game.load.spritesheet("theme","assets/character.png",63,100);
-        game.load.spritesheet("player","assets/dude.png",32,48);
+        game.load.spritesheet("playerwalk","assets/walkset.png",31.666,48);
+        game.load.spritesheet("playerrun","assets/runset.png",41.333,45);
         game.load.image("startBtn","assets/startBtn.png",173,38);
         game.load.image("tree","assets/tree.png",142,156);
+        game.load.image("dusty","assets/dusty.png",64,30);
     };
     this.create = function() {
-        game.state.start('start');
+       // game.state.start('start');
+        game.state.start('main');
     };
 };
 
 //开始页面
 game.States.start = function() {
-    var player,playerWalk,playerRun,title,startBtn;
+    var player,playerWalk,playerRun,title,startBtn,dusty;
     var bounceFlag=0;
     var moveFloat=false;
     var thisCreate=this;
+    var leftFlag=true;
     this.create = function() {
         //开启物理引擎
         game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -64,30 +68,29 @@ game.States.start = function() {
         //组标题整体居中
         title.x=game.world.centerX-252;
         //玩家精灵添加
-         player=game.add.sprite(-100,240,"player",5);
+         player=game.add.sprite(-100,240,"playerwalk",0);
         //使能玩家精灵物理引擎
         game.physics.arcade.enable(player);
         //玩家移动动画
-        player.animations.add("startMove",[5,6,7,8]);
-        player.animations.play("startMove",8,true);
+        player.animations.add("startMove",[3,4,5,6,7]);
+        player.animations.play("startMove",10,true);
         playerWalk=game.add.tween(player).to({x:132},3000,null,true);
         playerWalk.onComplete.add(this.wait,this);
-
         //开始按钮设置
         startBtn=game.add.button(game.world.centerX,game.world.centerY,"startBtn",this.nextStage,this);
         startBtn.anchor.setTo(0.5,-2);
         startBtn.alpha=0;
-
         //背景树添加
         var tree1=game.add.sprite(71,-78,"tree");
         tree1.angle=180;
         tree1.anchor.setTo(0.5,0.5);
         game.add.tween(tree1).to({y:78},2000,null,true);
-
         var tree2=game.add.sprite(game.world.width-142,game.world.height,"tree");
         game.add.tween(tree2).to({y:game.world.height-156},2000,null,true);
-
-
+        //起跳灰尘效果
+        dusty=game.add.sprite(720,288,"dusty");
+        dusty.alpha=0;
+        dusty.anchor.setTo(0.5,0.5);
     };
     this.update=function () {
         game.physics.arcade.collide(player,title,this.titleFloat,null,this);
@@ -96,6 +99,12 @@ game.States.start = function() {
             moveFloat=true;
             //按钮动画启动
             game.add.tween(startBtn).to({alpha:1},1000,null,true);
+            if(leftFlag){
+                player.animations.add("startBack",[8,9,10,11,12]);
+                player.animations.play("startBack",10,true);
+            };
+            leftFlag=false;
+            //console.log(game.time.totalElapsedSeconds().toFixed(3)-oldtime);
         };
         if(moveFloat){
             player.y=title.getChildAt(0).y+title.y-38;
@@ -104,28 +113,30 @@ game.States.start = function() {
     //player waits
     this.wait=function () {
         player.animations.stop("startMove");
-        player.frame=4;
+        player.frame=0;
         this.run();
     };
     //player runs
     this.run=function(){
         setTimeout(function () {
-            player.animations.add("startRun",[5,6,7,8]);
-            player.animations.play("startRun",8,true);
+            player.animations.add("startRun",[3,4,5,6,7]);
+            player.animations.play("startRun",10,true);
             playerRun=game.add.tween(player).to({x:700},3100,Phaser.Easing.Exponential.Out,true);
             playerRun.onComplete.add(thisCreate.back,this);
         },1300);
     };
     this.back=function () {
         player.animations.stop("startRun");
-        player.frame=4;
-        player.body.velocity.y=-160;
-        player.body.velocity.x=-72;
+        player.frame=1;
+        player.body.velocity.y=-147;
+        player.body.velocity.x=-77.5;
         player.body.bounce.y=0.4;
         player.body.gravity.y=80;
         player.body.collideWorldBounds=true;
-        player.animations.add("startBack",[0,1,2,3]);
-        player.animations.play("startBack",8,true);
+        //起跳灰尘效果播放
+        var dustyOn=game.add.tween(dusty).to({alpha:1},300,Phaser.Easing.Exponential.Out,false);
+        dustyOn.chain(game.add.tween(dusty).to({alpha:0},300,Phaser.Easing.Exponential.In,false));
+        dustyOn.start();
     };
     //标题浮动
     this.titleFloat=function(){
@@ -139,16 +150,15 @@ game.States.start = function() {
 
 //主场景
 game.States.main = function() {
+    var player;
     this.create = function() {
-        // 文本
-        var style = { font: "30px Arial", fill: "#ff0044", align: "center" };
-        var text=game.add.text(game.world.centerX, game.world.centerY, "Welcome to Main Stage", style);
-        text.anchor.setTo(0.5);
+        //开启物理引擎
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        //启动页面背景色
+        game.stage.backgroundColor="#ff9";
+        player=game.add.sprite(100,game.world.height-106,"playerwalk",0);
     };
 };
-
-
-
 
 //添加场景
 game.state.add('boot', game.States.boot);
